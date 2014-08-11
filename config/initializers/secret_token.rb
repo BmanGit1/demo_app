da8a0c0d104479fbbe3f80b0b58ca79336ccf7e1
Bman <bryantrlinares@yahoo.com>
2014-08-11 13:44:09 -0400

    Initial BRL commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '06884d90dc3cbba17e7d9dfe901cba6138308f0c8c6c79527f8e3ea8e10c4cabed19cee5b94c5c006489154af8b99b29f81998266ea4daa52eb462e5f2037036'
